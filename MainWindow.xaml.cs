180f3df83def730409ed515cd3a01e618aad3664
Lebo2308 <st10349188@rcconnect.edu.za>
2025-06-27 20:37:56 +0000

    created event handlers for the buttons and set the visibilty for each grid and what button is responsible for which button(set visibility to visible or hiddden to each respective button)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,5 +26,81 @@ namespace POE_Part3_Prog6221_chatbotapplication
             voice_greeting greeting = new voice_greeting(audioPath);
             greeting.PlayGreeting();
         }
+
+        //button to open the main page
+        private void open_main(object sender, RoutedEventArgs e)
+        {
+            //changing pages using the visible and hidden
+            //hide the logo_login grid when button is clicked
+            logo_login.Visibility = Visibility.Hidden;
+            //set the main page grid to be visible 
+            main_page.Visibility = Visibility.Visible;
+        }
+
+        //button to open the chatbot page in the main page
+        private void chats(object sender, RoutedEventArgs e)
+        {
+            //hide other pages and set the current one visible
+            reminder_page.Visibility = Visibility.Hidden;
+            quiz_page.Visibility = Visibility.Hidden;
+            activity_page.Visibility = Visibility.Hidden;
+
+            //current page and set it to be visible
+            chats_page.Visibility = Visibility.Visible;
+        }
+
+        //button to open the reminder page in the main page
+        private void reminder(object sender, RoutedEventArgs e)
+        {
+            //hide other pages and set the current one visible
+            chats_page.Visibility = Visibility.Hidden;
+            quiz_page.Visibility = Visibility.Hidden;
+            activity_page.Visibility = Visibility.Hidden;
+
+            //current page and set it to be visible
+            reminder_page.Visibility = Visibility.Visible;
+        }
+
+        //button to open the quiz/mini game page in the main page
+        private void quiz(object sender, RoutedEventArgs e)
+        {
+            //hide other pages and set the current one visible
+            chats_page.Visibility = Visibility.Hidden;
+            reminder_page.Visibility = Visibility.Hidden;
+            activity_page.Visibility = Visibility.Hidden;
+
+            //current page and set it to be visible
+            quiz_page.Visibility = Visibility.Visible;
+        }
+
+        //button to open the activity log page in the main page
+        private void activity(object sender, RoutedEventArgs e)
+        {
+            //hide other pages and set the current one visible
+            chats_page.Visibility = Visibility.Hidden;
+            reminder_page.Visibility = Visibility.Hidden;
+            quiz_page.Visibility = Visibility.Hidden;
+
+            //current page and set it to be visible
+            activity_page.Visibility = Visibility.Visible;
+        }
+
+        //button to logout/close the main page  to go to the login page
+        private void close_main(object sender, RoutedEventArgs e)
+        {
+            //hide the login grid when button is clicked
+            main_page.Visibility = Visibility.Hidden;
+            //set the chat page grid to be visible 
+            logo_login.Visibility = Visibility.Visible;
+
+        }
+
+        //button to exist the main program, exist the whole program
+        private void exits(object sender, RoutedEventArgs e)
+        {
+            //close the application
+            System.Environment.Exit(0);
+
+        }
     }
 }
