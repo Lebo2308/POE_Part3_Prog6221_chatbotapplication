6a4942626f574ee9ddf8cbdf4d1e56fa5af30f03
Lebo2308 <st10349188@rcconnect.edu.za>
2025-06-27 20:43:38 +0000

    declared some variables and added a constructor in the chatbot class

--- Chatbot.cs
+++ Chatbot.cs
@@ -1,6 +1,17 @@
 ﻿namespace POE_Part3_Prog6221_chatbotapplication
 {
-    internal class Chatbot
+    public class Chatbot
     {
+        // Dictionary to store keywords and associated chatbot replies
+        private Dictionary<string, List<string>> replies;
+        // List of common words to ignore during keyword matching
+        private List<string> ignore;
+        // Stores the user's name 
+        public string userName;
+
+        // Constructor initializes ignore list and keyword-reply dictionary
+        public Chatbot()
+        {
+        }//end of constructor
     }
 }
