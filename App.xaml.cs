2aeabc5bd6e91f779bdd511a9f839a31bf32655f
Lebo2308 <st10349188@rcconnect.edu.za>
2025-06-27 17:44:14 +0000

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace POE_Part3_Prog6221_chatbotapplication
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
