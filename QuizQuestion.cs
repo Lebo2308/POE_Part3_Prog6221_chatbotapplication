08d9a3a2ec2f32f45e52c158f9a23f37d04350b5
Lebo2308 <st10349188@rcconnect.edu.za>
2025-06-27 21:08:29 +0000

    setting the setter and getter in the QuizQuestion class

--- QuizQuestion.cs
+++ QuizQuestion.cs
@@ -1,6 +1,12 @@
 ﻿namespace POE_Part3_Prog6221_chatbotapplication
 {
-    internal class QuizQuestion
+    public class QuizQuestio
     {
+            //public getters and setters
+            public string Question { get; set; }
+            public string CorrectChoice { get; set; }
+
+            //get and set on the list 
+            public List<string> Choices { get; set; }
     }
 }
